f616964350c8dc93f426f46d8d2957a7740aa49c
Soproma2 <nikolozsopromadze@gmail.com>
2025-04-05 16:33:18 +0400

    .

--- src/app/car-card/car-card.component.ts
+++ src/app/car-card/car-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { CarService } from '../car.service';
 import { CommonModule } from '@angular/common';
 
